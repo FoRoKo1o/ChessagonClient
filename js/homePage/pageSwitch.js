e4639058ad161298eb307569ae06a661820dbb03
SEGMK <85564914+SEGMK@users.noreply.github.com>
2024-10-31 15:33:22 +0100

    fixed animation
    -now all elements whose position was fixed are animated to

--- js/homePage/pageSwitch.js
+++ js/homePage/pageSwitch.js
@@ -1,12 +1,28 @@
 var previousContainerElement = document.getElementById('homePage');
 var animationLength = 500;
+var searchedClass = 'fixedElementClass';
 function SwitchPages(/*string*/ containerName) {
     let containerElement = document.getElementById(containerName);
-    if(containerElement === previousContainerElement)
+    containerElement.getElementsByClassName
+    if (containerElement === previousContainerElement)
         return;
-    prepareElementToFadeIn(containerElement);
-    fadeOut(previousContainerElement);
-    fadeIn(containerElement);
+    let newElementsToAnimate = getChildElementsByClass(containerElement, searchedClass)
+    let oldElementsToAnimate = getChildElementsByClass(previousContainerElement, searchedClass);
+    for(let i = 0; i < newElementsToAnimate.length; i++)
+    {
+        prepareElementToFadeIn(newElementsToAnimate[i]);
+    }
+    for(let i = 0; i < oldElementsToAnimate.length; i++)
+    {
+        fadeOut(oldElementsToAnimate[i]);
+    }
+    for(let i = 0; i < newElementsToAnimate.length; i++)
+    {
+        fadeIn(newElementsToAnimate[i]);
+    }
+    // prepareElementToFadeIn(containerElement);
+    // fadeOut(previousContainerElement);
+    // fadeIn(containerElement);
     previousContainerElement = containerElement;
 }
 function fadeOut(/*element*/ container) {
@@ -20,15 +36,24 @@ function fadeOut(/*element*/ container) {
 }
 function fadeIn(/*element*/ container) {
     container.style.display = 'block';
-    setTimeout(function(){
+    setTimeout(function () {
         container.classList.add("defaultFadeState");
     }, 5);
-    setTimeout(function(){
+    setTimeout(function () {
         container.classList.remove("fadedIn");
     }, animationLength + 5);
 }
-function prepareElementToFadeIn(/*element*/ container)
-{
+function prepareElementToFadeIn(/*element*/ container) {
     container.classList.remove("defaultFadeState");
     container.classList.add("fadedIn");
+}
+function getChildElementsByClass(/*element*/ element, /*string*/ className) {
+    let matchedElements = [];
+    matchedElements.push(element);
+    for (var i = 0; i < element.childNodes.length; i++) {
+        if (String(element.childNodes[i].className).includes(className)) {
+            matchedElements.push(element.childNodes[i]);
+        }
+    }
+    return matchedElements;
 }
