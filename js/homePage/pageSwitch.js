4b28592c860ea75fd65a5dbc3a2b60bbef2a974d
SEGMK <85564914+SEGMK@users.noreply.github.com>
2024-10-31 14:57:34 +0100

    Added page switching animation
    -added page switching animation in JS
    -added needed css classes for animation support

--- js/homePage/pageSwitch.js
+++ js/homePage/pageSwitch.js
@@ -1,8 +1,34 @@
 var previousContainerElement = document.getElementById('homePage');
-function SwitchPages(/*string*/ containerName)
-{
+var animationLength = 500;
+function SwitchPages(/*string*/ containerName) {
     let containerElement = document.getElementById(containerName);
-    previousContainerElement.style.display = 'none';
-    containerElement.style.display = 'block';
+    if(containerElement === previousContainerElement)
+        return;
+    prepareElementToFadeIn(containerElement);
+    fadeOut(previousContainerElement);
+    fadeIn(containerElement);
     previousContainerElement = containerElement;
+}
+function fadeOut(/*element*/ container) {
+    container.classList.remove("defaultFadeState");
+    container.classList.add("fadedOut");
+    setTimeout(function () {
+        container.style.display = 'none';
+        container.classList.remove("fadedOut");
+        container.classList.add("defaultFadeState");
+    }, animationLength);
+}
+function fadeIn(/*element*/ container) {
+    container.style.display = 'block';
+    setTimeout(function(){
+        container.classList.add("defaultFadeState");
+    }, 5);
+    setTimeout(function(){
+        container.classList.remove("fadedIn");
+    }, animationLength + 5);
+}
+function prepareElementToFadeIn(/*element*/ container)
+{
+    container.classList.remove("defaultFadeState");
+    container.classList.add("fadedIn");
 }
